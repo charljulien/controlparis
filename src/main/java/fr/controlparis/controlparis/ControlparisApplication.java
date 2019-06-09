ac2ea031f1f145c11d6f79b3c5da5af6d41bbeb8
Charles <charljulien@gmail.com>
2019-06-09 08:40:49 +0200

    initial commit

---
+++ src/main/java/fr/controlparis/controlparis/ControlparisApplication.java
@@ -0,0 +1,13 @@
+package fr.controlparis.controlparis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlparisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlparisApplication.class, args);
+	}
+
+}
